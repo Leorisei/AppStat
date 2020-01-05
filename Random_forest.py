c05f96f6e754e247ef86e34124eebdc02df664f4
FELICES-David <58061130+FELICES-David@users.noreply.github.com>
2020-01-05 15:41:09 +0900

    Add files via upload

--- Random_forest.py
+++ Random_forest.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
+import csv
 
 data = pd.read_csv("train_test_2019.csv",encoding = 'unicode_escape')
 X = data[data.columns[:-1]]
@@ -40,3 +41,50 @@ print(C)
 P = C[1][1]/(C[1][1]+C[0][1])
 R = C[1][1]/(C[1][1]+C[1][0])
 print("F_Measure:",2*P*R/(P+R))
+
+data = pd.read_csv("submit_2019.csv",encoding = 'unicode_escape')
+X = data[data.columns[:-2]]
+
+cats = ['education','workclass','marital-status','occupation','relationship','race','sex','native-country']
+dummies = pd.get_dummies(X[cats])
+temp = pd.concat([X,dummies],axis=1)
+X = temp.drop(cats,axis=1)
+
+missing_columns = []
+for column in X_train.columns:
+    if column not in X.columns:
+        missing_columns.append(column)
+
+for column in missing_columns:
+    data[column] = 0
+
+X = data[data.columns]
+dummies = pd.get_dummies(X[['y (yes or no)  ','probability of yes (or score)']])
+temp = pd.concat([X,dummies],axis=1)
+X = temp.drop(['y (yes or no)  ','probability of yes (or score)'],axis=1)
+
+cats = ['education','workclass','marital-status','occupation','relationship','race','sex','native-country']
+dummies = pd.get_dummies(X[cats])
+temp = pd.concat([X,dummies],axis=1)
+X = temp.drop(cats,axis=1)
+
+Y_pred = RFClf.predict(X)
+Y_proba = RFClf.predict_proba(X)
+Y_proba_yes = [Y_proba[i][1] for i in range(len(Y_proba))]
+
+in_file = open("submit_2019.csv", "r")
+reader = csv.reader(in_file)
+out_file = open("submit_2019_result.csv", "w")
+writer = csv.writer(out_file)
+i = 0
+name = True
+for row in reader:
+    if name == False:
+        row[14] = Y_pred[i]
+        row[15] = Y_proba_yes[i]
+        i+=1
+    if name == True:
+        name = False
+    writer.writerow(row)
+in_file.close()    
+out_file.close()
